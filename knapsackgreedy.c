ae33ce1c54e22359edda350f3c579a5e80f793cb
yash-1116 <yash.23cs188@sode-edu.in>
2025-04-10 16:27:39 +0530

    Create knapsackgreedy.c

---
+++ knapsackgreedy.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#define MAX_ITEMS 10
+int i;
+double computeMaxValue(double W,double weight[],double value[],double ratio[],int nItems);
+
+int getNextItem(double weight[],double value[],double ratio[],int nItems);
+
+int main()
+{
+int nItems;
+double W;
+double weight[MAX_ITEMS];
+double value[MAX_ITEMS];
+double ratio[MAX_ITEMS];
+printf("Enter the number of items: ");
+scanf("%d",&nItems);
+
+printf("Enter the weights of the items: ");
+for(i=0;i<nItems;i++){
+scanf("%lf",&weight[i]);
+}
+
+printf("Enter the values/profits of the items: ");
+for(i=0;i<nItems;i++){
+scanf("%lf",&value[i]);
+}
+
+for(i=0;i<nItems;i++){
+ratio[i]=value[i]/weight[i];
+}
+
+printf("Enter the capacity of the knapsack: ");
+scanf("%lf",&W);
+
+printf("\nThe maximum value in a knapsack of capacity %0.2f is: %0.2f\n",W,
+computeMaxValue(W,weight,value,ratio,nItems));
+
+return 0;
+}
+
+double computeMaxValue(double W,double weight[],double value[],double ratio[],int nItems)
+{
+double cW=0;
+double cV=0;
+
+printf("\nItems consderable are: ");
+while(cW<W) {
+int item=getNextItem(weight,value,ratio,nItems);
+if(item==-1){
+break;
+}
+
+printf("%d",item+1);
+if(cW+weight[item]<=W){
+cW+=weight[item];
+cV+=value[item];
+ratio[item]=0;
+}else{
+cV+=(ratio[item]*(W-cW));
+cW+=(W-cW);
+break;
+}}
+return cV;
+}
+
+int getNextItem(double weight[],double value[],double ratio[],int nItems) {
+double highest=0;
+int index=-1;
+for(i=0;i<nItems;i++) {
+if(ratio[i]>highest){
+highest=ratio[i];
+index=i;
+}
+}
+printf("\t");
+return index;
+}
